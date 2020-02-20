116a27600092c0251e3c7c69f8218b3ac69dbea5
loveagri <282656050@qq.com>
2020-02-20 15:53:20 +0700

    msg

--- 45.php
+++ 45.php
@@ -11,7 +11,7 @@ if ($signature) {
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
     file_put_contents('./2.txt',$hash);
     file_put_contents('./3.txt',strcmp($signature, $hash));
-    file_put_contents('./6.txt',shell_exec("cd {$path} && ls -al"));
+    file_put_contents('./6.txt',shell_exec("cd {$path} && /usr/bin/git rest --hard origin/master && /usr/bin/git/ clean -f"));
     if (strcmp($signature, $hash) == 0) {
 //        echo shell_exec("cd {$path} && /usr/bin/git rest --hard origin/master && /usr/bin/git/ clean -f && /usr/bin/git pull 2>&1");
         shell_exec("cd {$path} && /usr/bin/git rest --hard origin/master && /usr/bin/git/ clean -f && /usr/bin/git pull");
