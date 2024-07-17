e466de6db299201d39dc1ac7ffce7f9722fe3771
loveagri <282656050@qq.com>
2024-07-17 21:12:07 +0800

    build: commit

--- docs/.vitepress/configs/search/algolia-search.ts
+++ docs/.vitepress/configs/search/algolia-search.ts
@@ -1,50 +1,50 @@
-import type { AlgoliaSearchOptions } from 'vitepress';
+import type {AlgoliaSearchOptions} from 'vitepress';
 
 export const algoliaSearchOptions: AlgoliaSearchOptions = {
-  appId: '54QBTYONYB',
-  apiKey: '4d7687dbed0b5252b102f0a79a1f2b0a',
-  indexName: 'loveagri',
-  locales: {
-    root: {
-      placeholder: '搜索文档',
-      translations: {
-        button: {
-          buttonText: '搜索文档',
-          buttonAriaLabel: '搜索文档'
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: '清除查询条件',
-            resetButtonAriaLabel: '清除查询条件',
-            cancelButtonText: '取消',
-            cancelButtonAriaLabel: '取消'
-          },
-          startScreen: {
-            recentSearchesTitle: '搜索历史',
-            noRecentSearchesText: '没有搜索历史',
-            saveRecentSearchButtonTitle: '保存至搜索历史',
-            removeRecentSearchButtonTitle: '从搜索历史中移除',
-            favoriteSearchesTitle: '收藏',
-            removeFavoriteSearchButtonTitle: '从收藏中移除'
-          },
-          errorScreen: {
-            titleText: '无法获取结果',
-            helpText: '你可能需要检查你的网络连接'
-          },
-          footer: {
-            selectText: '选择',
-            navigateText: '切换',
-            closeText: '关闭',
-            searchByText: '搜索提供者'
-          },
-          noResultsScreen: {
-            noResultsText: '无法找到相关结果',
-            suggestedQueryText: '你可以尝试查询',
-            reportMissingResultsText: '你认为该查询应该有结果？',
-            reportMissingResultsLinkText: '点击反馈'
-          }
-        }
-      }
-    }
-  }
+	appId: '4OY6VO2E9N',
+	apiKey: '7a1c2f8a02855bf3c85e3499c0f8365f',
+	indexName: 'loveagri',
+	locales: {
+		root: {
+			placeholder: '搜索文档',
+			translations: {
+				button: {
+					buttonText: '搜索文档',
+					buttonAriaLabel: '搜索文档'
+				},
+				modal: {
+					searchBox: {
+						resetButtonTitle: '清除查询条件',
+						resetButtonAriaLabel: '清除查询条件',
+						cancelButtonText: '取消',
+						cancelButtonAriaLabel: '取消'
+					},
+					startScreen: {
+						recentSearchesTitle: '搜索历史',
+						noRecentSearchesText: '没有搜索历史',
+						saveRecentSearchButtonTitle: '保存至搜索历史',
+						removeRecentSearchButtonTitle: '从搜索历史中移除',
+						favoriteSearchesTitle: '收藏',
+						removeFavoriteSearchButtonTitle: '从收藏中移除'
+					},
+					errorScreen: {
+						titleText: '无法获取结果',
+						helpText: '你可能需要检查你的网络连接'
+					},
+					footer: {
+						selectText: '选择',
+						navigateText: '切换',
+						closeText: '关闭',
+						searchByText: '搜索提供者'
+					},
+					noResultsScreen: {
+						noResultsText: '无法找到相关结果',
+						suggestedQueryText: '你可以尝试查询',
+						reportMissingResultsText: '你认为该查询应该有结果？',
+						reportMissingResultsLinkText: '点击反馈'
+					}
+				}
+			}
+		}
+	}
 };
