165fb6e540e7c9c04d6438ac6957034923b32c5a
loveagri <282656050@qq.com>
2024-07-18 10:47:50 +0800

    build: commit

--- docs/.vitepress/configs/theme.ts
+++ docs/.vitepress/configs/theme.ts
@@ -46,7 +46,8 @@ export const themeConfig: DefaultTheme.Config = {
 	// },
 
 	footer: {
-		message: '<p  style="display: flex;justify-content: center;color: #3eaf7c;"><svg style="width: 16px"' +
+		message: '<div style="display: flex; flex-direction:column;justify-content: center;align-items:center;color:' +
+			' #3eaf7c; "><p style="display: flex; justify-content: center;align-items:center"><svg style="width: 16px"' +
 			' data-v-33cfc005="" viewBox="0 0 1024 1024" version="1.1"' +
 			' xmlns="http://www.w3.org/2000/svg"><title data-v-33cfc005="">ICP备案号</title><path data-v-33cfc005=""' +
 			' d="M778.24 163.84c-76.8-40.96-165.888-61.44-269.312-61.44s-192.512 20.48-269.312 61.44h-133.12l23.552' +
@@ -66,10 +67,8 @@ export const themeConfig: DefaultTheme.Config = {
 			' m-150.528 194.56h94.208v41.984h-94.208v-41.984z m0 78.848h94.208v41.984h-94.208v-41.984z' +
 			' m-144.384-78.848h94.208v41.984H390.144v-41.984z m0 78.848h94.208v41.984H390.144v-41.984zM424.96' +
 			' 326.656h182.272c-26.624 22.528-57.344 41.984-94.208' +
-			' 57.344-31.744-15.36-61.44-34.816-88.064-57.344z"></path></svg>冀ICP备19024518号-1<br/><img' +
-			' style="width:16px;height: 16px" src="/gongan.png" title="联网备案号">京公网安备11010802043456</p>' +
-			'<p style="display: flex;justify-content: center; align-items:center;color: #3eaf7c;">' +
-			'<img style="width:16px;height: 16px" src="/gongan.png" title="联网备案号">京公网安备11010802043456</p>',
+			' 57.344-31.744-15.36-61.44-34.816-88.064-57.344z"></path></svg>冀ICP备19024518号-1</p>' +
+			'<p style="display: flex; justify-content: center;align-items:center"><img style="width:16px;height: 16px" src="/gongan.png" title="联网备案号">京公网安备11010802043456</p></div>',
 		copyright: `<p style="display: flex;justify-content: center; align-items:center;color: #3eaf7c;">Copyright © 2019-${new Date().getFullYear()} loveagri</p>`
 	},
 
